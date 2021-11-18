8e3551538a3d829bfcccb7256123aba9abe93341
Gladwin <gladwinyjh@gmail.com>
2021-11-18 20:53:15 +0800

    Cleanup

--- python/AboveAverage.py
+++ python/AboveAverage.py
@@ -1,29 +1,11 @@
 import math
 
-# def calArea(walkArr, rideArr, intArr):
-#     elapsed = 0
-
-#     for i in range(len(walkArr)-1):
-#         elapsed += walkArr[i]
-
-#         if (elapsed%intArr[i] != 0):
-#             elapsed += (intArr[i] - elapsed%intArr[i])
-        
-        
-#         elapsed += rideArr[i]
-    
-#     elapsed += walkArr[-1]
-
-#     return elapsed
-        
 
 c = int(input())
 
 for i in range(c):
     sum = 0
-    
     arr = list(map(int, input().split()))
-
     n = arr.pop(0)
 
     for score in arr:
@@ -38,5 +20,4 @@ for i in range(c):
 
     perc = (count/len(arr)) * 100
 
-    print("%.3f" % perc + "%")
-        
+    print("%.3f" % perc + "%")        
